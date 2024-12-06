bf468cf92bf0436440771dfdda66e575af06f20a
Aru Poleo Vargas <aru@Arus-Mac-mini.local>
2024-12-05 20:46:17 -0500

    added gh workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,8 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	site: 'https://arupv.github.io',
+	base: '/astro-blog'
+
+});
